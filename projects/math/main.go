11f03f16292457e006516376cab43a6bcecd9803
dashadarklord2005 <dariaserezhina@gmail.com>
2024-12-19 17:21:24 +0300

    Update main.go

--- projects/math/main.go
+++ projects/math/main.go
@@ -1,7 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
+
+var k float64
+var p float64
+var v float64
+
+func M() float64 {
+	return p * v
+}
+func W() float64 {
+	return math.Sqrt(k / M())
+}
+func T() float64 {
+	return 6 / W()
+}
 
 func main() {
-	fmt.Println("Hello, world!!!")
+	fmt.Scan(&k)
+	fmt.Scan(&p)
+	fmt.Scan(&v)
+	fmt.Println(T())
 }
