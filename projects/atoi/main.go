32dacb4720c4d765d59ce22dc84868ad1565f983
dashadarklord2005 <dariaserezhina@gmail.com>
2024-12-19 17:26:03 +0300

    Update main.go

--- projects/atoi/main.go
+++ projects/atoi/main.go
@@ -1,7 +1,25 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+)
+
+func squareDigits(num int) string {
+	result := ""
+	numStr := strconv.Itoa(num)
+
+	for _, digit := range numStr {
+		d, _ := strconv.Atoi(string(digit))
+		squared := d * d
+		result += strconv.Itoa(squared)
+	}
+
+	return result
+}
 
 func main() {
-	fmt.Println("Hello, world!!!")
+	var input int
+	fmt.Scan(&input) // Читаем только число
+	fmt.Println(squareDigits(input)) // Печатаем только результат
 }
